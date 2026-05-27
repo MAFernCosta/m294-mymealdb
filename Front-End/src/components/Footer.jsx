71a79107da0b4293555cae6e5820592d518b53cc
Miguel Costa <maferncosta@gmail.com>
2026-05-27 11:51:01 +0200

    Implemented Footer with style

--- Front-End/src/components/Footer.jsx
+++ Front-End/src/components/Footer.jsx
@@ -1,7 +1,34 @@
-function Footer(){
+import { Link } from "react-router-dom";
+
+function Footer() {
+
     return (
         <>
-        <p>footer</p>
+            <div className="container">
+                <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                    <div className="col-md-4 d-flex align-items-center">
+                        <span className="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc</span>
+                    </div>
+                    <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                        <li className="ms-3">
+                            <Link
+                                className="pe-2 link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover link-offset-3"
+                                to="/"
+                                aria-label="Home"
+                            >
+                                Home
+                            </Link>
+                            <Link
+                                className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover link-offset-3"
+                                to="/about"
+                                aria-label="About"
+                            >
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
